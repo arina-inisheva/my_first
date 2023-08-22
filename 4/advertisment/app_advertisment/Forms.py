fa23f4b41c7cd29fd7d44bb492d4743b825eee96
arina-inisheva <arina.inisheva@yandex.ru>
2023-08-22 19:49:56 +0300

    Урок 10, задание 4, номер 2

--- 4/advertisment/app_advertisment/Forms.py
+++ 4/advertisment/app_advertisment/Forms.py
@@ -1,14 +1,24 @@
-from django.forms import ModelForm, TextInput, Textarea, NumberInput, CheckboxInput, FileInput
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, TextInput, Textarea, NumberInput, CheckboxInput, FileInput, CharField
 
 from app_advertisment.models import Advertisement
 
 
+def title_validator(value):
+    if value.startswith('?'):
+        raise ValidationError(
+            message='Заголовок не должен начинаться с знака вопроса.'
+        )
+
+
 class DBAdvertisementForm(ModelForm):
+    title = CharField(max_length=64, widget=TextInput(attrs={'class': 'form-control form-control-lg'}),
+                      label='Заголовок', validators=[title_validator])
+
     class Meta:
         model = Advertisement
         fields = ['title', 'description', 'price', 'auction', 'image']
         labels = {
-            'title': 'Заголовок',
             'description': 'Описание',
             'price': 'Цена',
             'auction': 'Возможность торга',
@@ -16,13 +26,14 @@ class DBAdvertisementForm(ModelForm):
 
         }
         widgets = {
-            'title': TextInput(attrs={'class': 'form-control form-control-lg'}),
             'description': Textarea(attrs={'class': 'form-control form-control-lg'}),
             'price': NumberInput(attrs={'class': 'form-control form-control-lg'}),
             'auction': CheckboxInput(attrs={'class': 'form-check-input'}),
             'image': FileInput(attrs={'class': 'form-control form-control-lg'})
 
         }
+
+
 # title = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}), label='Заголовок')
     # description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control form-control-lg'}), label='Описание')
     # price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control form-control-lg'}),
